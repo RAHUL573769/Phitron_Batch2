65039b9d545452bfe102510da6913d73a7d0821f
Rahul Rudra <rahulrudra665@gmail.com>
2024-01-04 08:06:25 +0600

    for loop started

---
+++ IntroductionToProgrammingLanguage/Week-03/forLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int startValue = 0;
+    for (startValue = 0; startValue < 10; startValue++)
+    {
+        printf("Hello World \n");
+    }
+
+    return 0;
+}
---
+++ IntroductionToProgrammingLanguage/Week-03/whileLooop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int start = 0;
+    while (start < 10)
+    {
+        printf("Hello World \n");
+        start++;
+    }
+
+    return 0;
+}
