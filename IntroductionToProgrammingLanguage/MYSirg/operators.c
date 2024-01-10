6e797eabec502e7876c85eb0825c637d96b2a647
Rahul Rudra <rahulrudra665@gmail.com>
2024-01-10 14:14:46 +0600

    changes commit

---
+++ IntroductionToProgrammingLanguage/MYSirg/operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int value1 = 65, value2 = 9, sum = 0;
+    if (value1 > value2)
+    {
+        printf("%d", value1);
+    }
+    else
+    {
+        printf("%d", value2);
+    }
+
+    return 0;
+}
